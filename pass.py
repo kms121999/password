3ad8f7500061638d9c112446f5dbd0ee9a0a9eea
Keaton <ksmith36@student.waubonsee.edu>
2019-03-05 11:16:49 -0600

    Added the Attempts Counter

--- pass.py
+++ pass.py
@@ -5,8 +5,11 @@ PASSWORD_PROMPT = "Password: "
 
 userAttempt = input(PASSWORD_PROMPT)
 
+failedAttempts = 0
 while userAttempt != PASSWORD:
+    failedAttempts += 1
     print("That is incorrect. >:(")
+    print("You have guessed incorectly", failedAttempts, "times.")
 
     userAttempt = input(PASSWORD_PROMPT)
 
